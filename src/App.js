731e1ff138f239239adfbc53bc532e032ce24ca5
zihadulIslam2 <zihadul708@gmail.com>
2025-01-14 15:28:19 +0600

    basic maptailer

--- src/App.js
+++ src/App.js
@@ -18,52 +18,53 @@ export default function App() {
       style={{ width: '100vw', height: '100vh' }}
     >
       <TileLayer
-        url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=YOUR_KEY"
+        url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=MOD8vizaYbqIf37j9HBJ"
         attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
       />
-      {
-        statesData.features.map((state) => {
-          const coordinates = state.geometry.coordinates[0].map((item) => [item[1], item[0]]);
+      {statesData.features.map((state) => {
+        const coordinates = state.geometry.coordinates[0].map((item) => [
+          item[1],
+          item[0],
+        ])
 
-          return (<Polygon
+        return (
+          <Polygon
             pathOptions={{
               fillColor: '#FD8D3C',
               fillOpacity: 0.7,
               weight: 2,
               opacity: 1,
               dashArray: 3,
-              color: 'white'
+              color: 'white',
             }}
             positions={coordinates}
             eventHandlers={{
               mouseover: (e) => {
-                const layer = e.target;
+                const layer = e.target
                 layer.setStyle({
-                  dashArray: "",
-                  fillColor: "#BD0026",
+                  dashArray: '',
+                  fillColor: '#BD0026',
                   fillOpacity: 0.7,
                   weight: 2,
                   opacity: 1,
-                  color: "white",
+                  color: 'white',
                 })
               },
               mouseout: (e) => {
-                const layer = e.target;
+                const layer = e.target
                 layer.setStyle({
                   fillOpacity: 0.7,
                   weight: 2,
-                  dashArray: "3",
+                  dashArray: '3',
                   color: 'white',
-                  fillColor: '#FD8D3C'
-                });
+                  fillColor: '#FD8D3C',
+                })
               },
-              click: (e) => {
-
-              }
+              click: (e) => {},
             }}
-          />)
-        })
-      }
+          />
+        )
+      })}
     </MapContainer>
-  );
+  )
 }
